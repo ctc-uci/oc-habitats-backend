f56171d09db80c4ab0f1d7c0232dc4a2bacc9584
Parsa Karamat <pkmtyp@gmail.com>
2021-11-17 13:45:39 -0800

    Adding user schema

---
+++ models/user.schema.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  userId: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  isAdmin: Boolean,
+  isSuperAdmin: Boolean,
+  isActive: Boolean,
+  segments: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Segments'
+  }],
+});
+
+module.exports = mongoose.model('User', UserSchema);
