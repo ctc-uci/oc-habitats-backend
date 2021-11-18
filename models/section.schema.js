4aedc172dd42da9788218808c8d5ec1eca7c4c2c
Phillip Chan <chnphillip@gmail.com>
2021-11-18 07:55:00 -0800

    wip  seperated schemas + refined

--- models/section.schema.js
+++ models/section.schema.js
@@ -1,32 +1,14 @@
+import segmentSchema from "./segment.schema";
 const mongoose = require("mongoose");
 
-const sectionSchema = new mongoose.Schema(
-  {
-    _id: { type: String, required: true, unique: true },
-    name: { type: String, required: true, unique: true },
-    geofence_area: {
-      latitude: Number,
-      longitude: Number,
-    },
-    segments: [
-      {
-        id: { type: String, required: true, unique: true },
-        name: { type: String, required: true, unique: true },
-        geofence_area: {
-          latitude: Number,
-          longitude: Number,
-        },
-        deadline: Date,
-        volunteers: [
-          {
-            id: ObjectId,
-            name: String,
-          },
-        ],
-      },
-    ],
+const sectionSchema = new mongoose.Schema({
+  _id: String,
+  name: { type: String, required: true, unique: true },
+  geofence_area: {
+    latitude: Number,
+    longitude: Number,
   },
-  { _id: false } // Enable custom _id for sections
-);
+  segments: [segmentSchema],
+});
 
 module.exports = mongoose.model("Section", sectionSchema);
---
+++ models/segment.schema.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const segmentSchema = new mongoose.Schema({
+  _id: String,
+  name: { type: String, required: true, unique: true },
+  description: String,
+  geofence_area: {
+    latitude: Number,
+    longitude: Number,
+  },
+  deadline: Date,
+  volunteers: [
+    {
+      id: ObjectId,
+      name: String,
+    },
+  ],
+});
+
+module.exports = mongoose.model("Segment", segmentSchema);
