d05c666a2c343296d0299bc066f7199b2c8c3d4a
Jacqueline Thai <57199142+Jacqueline1201@users.noreply.github.com>
2021-11-18 20:27:11 -0800

    Update routes/models/och_log_entry_schema.js
    Co-authored-by: ctc-devops <90984711+ctc-devops@users.noreply.github.com>

--- routes/models/och_log_entry_schema.js
+++ routes/models/och_log_entry_schema.js
@@ -5,7 +5,7 @@ const {Schema} = mongoose;
 const logEntry = new Schema({
   isMonitorLog: Boolean, // MonitorLog/CheckIn? Yes portion, but what is the BetterImpact thing?
   monitorLogId: String, // Log No. 04-15-21.OC9a.Stacey Chartier-Grable  -- generate from date,habitat code (survey Location/Segment), surveyor name
-    title:  String, // Coastal Dune Habitat Survey Log
+  title: String, // Coastal Dune Habitat Survey Log
     surveyLocation: String, // OC9a
     surveyorName: String, // Stacey Chartier-Grable
     surveyDate: Date, // 4-15-21
