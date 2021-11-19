8b5a1c47a0e88c5740709c2182be8732fbad3890
Jacqueline Thai <57199142+Jacqueline1201@users.noreply.github.com>
2021-11-18 20:28:18 -0800

    Update routes/models/och_log_entry_schema.js
    Co-authored-by: ctc-devops <90984711+ctc-devops@users.noreply.github.com>

--- routes/models/och_log_entry_schema.js
+++ routes/models/och_log_entry_schema.js
@@ -15,7 +15,7 @@ const logEntry = new Schema({
     
     temperature: Number,
   cloudCover: Number, // do they want int -> percentages? 0 = 0% , 1 = 33%, 2 = 66%, 3 = 100%
-    precipitation: {
+  precipitation: {
         type: String,
     enum: ['None', 'Fog', 'Drizzle', 'Rain'],
         default: 'None'
