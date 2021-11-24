da28ce4b39f3f6d3761bb2969eb69058e20b4d56
Parsa Karamat <pkmtyp@gmail.com>
2021-11-23 22:29:40 -0800

    Updated Schema to comply with ESLint

--- models/user.schema.js
+++ models/user.schema.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
   isTrainee: Boolean,
   segments: [
     {
-      type: Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: 'Segments',
     },
   ],
