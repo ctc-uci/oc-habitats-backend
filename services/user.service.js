0e47e44a24bd056097189eb442b26308f5cc6afa
Melayna Navarro <mjnavarro0089@gmail.com>
2022-01-19 16:49:19 -0800

    fixing eslint errors

--- services/user.service.js
+++ services/user.service.js
@@ -1,29 +1,29 @@
 const UserModel = require('../models/user.schema');
 
 const getProfile = async (profileId) => {
-    return await UserModel.findOne({ profileId });
-}
+  return UserModel.findOne({ profileId });
+};
 
 const getAllProfiles = async () => {
-    return await UserModel.find({});
-}
+  return UserModel.find({});
+};
 
 const updateProfile = async (profileId, updatedProfile) => {
-    return await UserModel.updateOne(
-        { profileId },
-        {
-            $set: updatedProfile,
-        }
-    );
-}
+  return UserModel.updateOne(
+    { profileId },
+    {
+      $set: updatedProfile,
+    },
+  );
+};
 
 const deleteProfile = async (profileId) => {
-    return UserModel.remove({ profileId });
-}
+  return UserModel.remove({ profileId });
+};
 
 module.exports = {
-    getProfile,
-    getAllProfiles,
-    updateProfile,
-    deleteProfile
-}
+  getProfile,
+  getAllProfiles,
+  updateProfile,
+  deleteProfile,
+};
