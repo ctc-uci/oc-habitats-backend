939cd805006f42234f40239f16ca4e0d5612b545
Phillip Chan <chnphillip@gmail.com>
2022-03-23 00:08:46 -0700

    fix deleteSection service

--- services/section.segment.service.js
+++ services/section.segment.service.js
@@ -30,8 +30,22 @@ module.exports = {
   updateSegment: (id, updatedSegment) => {
     return Segment.findByIdAndUpdate(id, updatedSegment, options);
   },
-  deleteSection: (id) => {
-    return Section.findByIdAndDelete(id);
+  deleteSection: async (id) => {
+    const results = { section: null, segment: null };
+    const res = await Section.findById(id);
+    const segList = res.segments.map((seg) => {
+      return seg.name;
+    });
+
+    results.segment = Promise.all(
+      segList.forEach(async (seg) => {
+        await Segment.findByIdAndUpdate(seg, { assigned: false });
+      }),
+    ).catch((err) => {
+      return err;
+    });
+    results.section = await Section.findByIdAndDelete(id);
+    return results;
   },
   deleteSegment: async (segmentID, sectionName) => {
     const results = { section: null, segment: null };
