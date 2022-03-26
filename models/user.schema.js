fc70f90585f6a99df5c7b90c3b7da8c381e4e909
Zubair Sidhu <sidhuzubair@gmail.com>
2022-03-25 18:39:27 -0700

    Enable virtuals for user schema

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,25 +1,28 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  _id: String,
-  firstName: String,
-  lastName: String,
-  email: String,
-  password: String,
-  isAdmin: Boolean,
-  isSuperAdmin: Boolean,
-  isActive: Boolean,
-  isTrainee: Boolean,
-  profileImage: {
-    data: Buffer,
-    contentType: String,
-  },
-  segments: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Segments',
+const userSchema = new mongoose.Schema(
+  {
+    _id: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    isAdmin: Boolean,
+    isSuperAdmin: Boolean,
+    isActive: Boolean,
+    isTrainee: Boolean,
+    profileImage: {
+      data: Buffer,
+      contentType: String,
     },
-  ],
-});
+    segments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Segments',
+      },
+    ],
+  },
+  { toJSON: { virtuals: true } },
+);
 
 module.exports = mongoose.model('User', userSchema);
