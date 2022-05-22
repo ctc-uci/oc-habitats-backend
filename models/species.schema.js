7652a9ed8be26f200e40769762e0e572281a2e3f
Ted Kim <kimgeobuk@gmail.com>
2022-05-22 16:27:49 -0700

    refactored species categorization in backend

--- models/species.schema.js
+++ models/species.schema.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const speciesSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   code: { type: String, required: true, unique: true },
-  isListed: { type: Boolean, default: false },
-  isPredator: { type: Boolean, default: false },
-  // isNeither covers the case where the predator is neither listed or non listed
-  isNeither: Boolean,
+  category: {
+    type: String,
+    enum: ['NON_LISTED_PREDATOR', 'JUST_PREDATOR', 'LISTED', 'NON_LISTED'],
+  },
 });
 
 module.exports = mongoose.model('Species', speciesSchema);
