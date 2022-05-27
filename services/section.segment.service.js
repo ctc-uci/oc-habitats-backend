b9baa23153be0905327244479d65f18b07b3e150
Jane Vo <vo.janehanh@gmail.com>
2022-05-26 21:35:39 -0700

    scuffed work around

--- services/section.segment.service.js
+++ services/section.segment.service.js
@@ -111,6 +111,8 @@ module.exports = {
 
   deleteSegment: async (segmentID, sectionId) => {
     const results = { section: null, segment: null, volunteers: null };
+    const volunteers = await Segment.findOne({ _id: segmentID }, { _id: 0, volunteers: 1 });
+
     results.segment = await Segment.findByIdAndDelete(segmentID);
     // delete from Section
     results.section = await Section.findOneAndUpdate(
@@ -121,11 +123,19 @@ module.exports = {
         },
       },
     );
+
     // delete form Users that are assigned this segment
-    results.volunteers = await User.updateMany(
-      { firebaseId: { $in: results.segment.volunteers } },
-      { $pull: { segments: segmentID } },
-    );
+    if (volunteers && volunteers.volunteers.length !== 0) {
+      try {
+        results.volunteers = await User.updateMany(
+          { firebaseId: { $in: volunteers.volunteers } },
+          { $pull: { segments: segmentID } },
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
     return results;
   },
 };
