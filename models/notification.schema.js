b8d794c8413e44c790f75c63e517a1a98b423ad3
Jane Vo <70874803+janehvo@users.noreply.github.com>
2022-07-08 00:55:41 -0700

    Notification fixes (#84)
    * Remove notification type
    
    * Change notification schema

--- models/notification.schema.js
+++ models/notification.schema.js
@@ -2,14 +2,9 @@ const mongoose = require('mongoose');
 
 const notificationSchema = new mongoose.Schema({
   message: String,
-  title: String,
   userId: {
     type: String,
     ref: 'User',
-  },
-  type: {
-    type: String,
-    enum: ['MONITOR_LOG_APPROVED', 'CHANGES_REQUESTED'],
   },
 });
 
